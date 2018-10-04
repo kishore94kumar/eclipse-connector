f3d03abf2f7a0b378832813344d2f443e2076462
Admin <Admin@dell>
2018-10-04 08:27:39 +0530

    commiting staging unit file

---
+++ JunitQuickStart/src/test/java/com/junit/string/test/StringTest.java
@@ -0,0 +1,14 @@
+package com.junit.string.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class StringTest {
+
+	@Test
+	public void test() {
+		assertEquals(3,3);
+	}
+
+}
